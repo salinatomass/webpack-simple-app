4849c2ade6437b430a4851483039da5287080c81
Tomás Salina <salinatomass53@gmail.com>
2021-09-29 22:55:56 -0300

    file optimization and minification

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
@@ -35,7 +37,7 @@ module.exports = {
         test: /\.(woff|woff2)$/i,
         type: "asset/resource",
         generator: {
-          filename: "assets/fonts/[hash][ext][query]",
+          filename: "assets/fonts/[contenthash][ext][query]",
         },
       },
     ],
@@ -46,7 +48,9 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].[contenthash].css",
+    }),
     new CopyPlugin({
       patterns: [
         {
@@ -56,4 +60,8 @@ module.exports = {
       ],
     }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
 };
